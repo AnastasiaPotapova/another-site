a46595fa88ec221fa0801aa50454bcaeaa32f080
anastasiapotapova <nast-pota@yandex.ru>
2020-05-09 18:26:52 +0400

    Version 1.0 -работает система подписок Todo -сообщения -фото -стиль получше -посты

--- models.py
+++ models.py
@@ -2,13 +2,31 @@ from app import db
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+dialogs = db.Table(
+    'dialogs',
+    db.Column('sender_id', db.Integer, db.ForeignKey('messages.id')),
+    db.Column('recipient_id', db.Integer, db.ForeignKey('messages.id'))
+)
+
 
 class User(db.Model):
-    __tablename__ = 'users'
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    about_me = db.Column(db.String(140))
+    followed = db.relationship(
+        'User', secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
@@ -19,23 +37,37 @@ class User(db.Model):
     def check_password(self, password):
         return self.password_hash == password
 
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def is_following(self, user):
+        return self.followed.filter(
+            followers.c.followed_id == user.id).count() > 0
+
 
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
 
 
-class Massage(db.Model):
+class Message(db.Model):
+    __tablename__ = 'messages'
     id = db.Column(db.Integer, primary_key=True)
+    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    from_id = db.Column(db.Integer)
-    to_id = db.Column(db.Integer)
 
     def __repr__(self):
-        return '<Post {}>'.format(self.body)
+        return '<Message {}>'.format(self.body)
+
--- routes.py
+++ routes.py
@@ -1,13 +1,31 @@
 from flask import render_template, redirect, request, session, flash
+from flask_login import current_user, login_required
 from forms import RegistrForm, LoginForm
 from app import app, db
 from models import User
 
 
+@app.route('/logout')
+def logout():
+    session['user_id'] = None
+    return redirect('/login')
+
+
+@app.route("/users", methods=['POST', 'GET'])
+def users():
+    user = User.query
+    print(list(user))
+    return render_template('users.html', title='Страница', users=list(user))
+
+
 @app.route("/index/<int:id>", methods=['POST', 'GET'])
 def index(id):
     user = User.query.filter_by(id=id).first()
-    return render_template('index.html', title='Страница', user=user)
+    me = User.query.filter_by(id=session['user_id']).first()
+    if session['user_id'] == id:
+        return render_template('index.html', title='Страница', user=user, me=0)
+    else:
+        return render_template('index.html', title='Страница', user=user, me=me)
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -21,6 +39,7 @@ def login():
         if user is None or not user.check_password(password):
             flash('Invalid username or password')
         else:
+            session['user_id'] = user.id
             return redirect('/index/{}'.format(user.id))
     return render_template('login.html', title='Вход', form=form)
 
@@ -38,3 +57,45 @@ def register():
         db.session.commit()
         return redirect('/login')
     return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/follow/<int:userid>', methods=['GET', 'POST'])
+def follow(userid):
+    user = User.query.filter_by(id=userid).first()
+    me = User.query.filter_by(id=session['user_id']).first()
+    if user is None:
+        return redirect('/login')
+    if user.id == session['user_id']:
+        return redirect('/index/{}'.format(userid))
+    if me.is_following(user):
+        return redirect('/index/{}'.format(userid))
+    me.follow(user)
+    db.session.commit()
+    return redirect('/index/{}'.format(userid))
+
+
+@app.route('/unfollow/<int:userid>', methods=['GET', 'POST'])
+def unfollow(userid):
+    user = User.query.filter_by(id=userid).first()
+    me = User.query.filter_by(id=session['user_id']).first()
+    if user is None:
+        return redirect('/login')
+    if user == me:
+        return redirect('/index/{}'.format(userid))
+    me.unfollow(user)
+    db.session.commit()
+    return redirect('/index/{}'.format(userid))
+
+
+@app.route('/followers', methods=['GET', 'POST'])
+def followers():
+    me = User.query.filter_by(id=session['user_id']).first()
+    users = list(me.followers)
+    return render_template('users.html', title='Подписчики', users=users)
+
+
+@app.route('/followed', methods=['GET', 'POST'])
+def followed():
+    me = User.query.filter_by(id=session['user_id']).first()
+    users = list(me.followed)
+    return render_template('users.html', title='Подписчики', users=users)
